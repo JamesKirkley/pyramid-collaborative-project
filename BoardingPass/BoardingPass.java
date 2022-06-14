8131ec787174f6623c02f126e37d6c13748c01dc
Marcus Tisdale <98255081+mstisdale@users.noreply.github.com>
2022-06-14 12:48:45 -0700

    Add files via upload

---
+++ BoardingPass/BoardingPass.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.time.LocalDate;
+
+
+public class BoardingPass {
+private String name;
+private String email;
+private int phone;
+private char gender;
+private int age;
+private float passnumber;
+private float eta;
+private float departure;
+
+
+
+}
